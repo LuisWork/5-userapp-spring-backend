d09bb1a2949321e4d3a0e0450c39d75eb3792c2f
Luis Zambrano <LuisZambrano.Work@gmail.com>
2024-06-16 02:36:47 -0400

    Implementando metodos POST PUT DELETE y @RequestMapping

--- src/main/java/com/springboot/backend/luis/usersapp/users_backend/controllers/UserController.java
+++ src/main/java/com/springboot/backend/luis/usersapp/users_backend/controllers/UserController.java
@@ -6,16 +6,21 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.backend.luis.usersapp.users_backend.entities.User;
 import com.springboot.backend.luis.usersapp.users_backend.services.UserService;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
@@ -36,6 +41,39 @@ public class UserController {
         // return ResponseEntity.notFound().build();
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(Collections.singletonMap("Error", "El usuario no se encontro por ID: " + id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        Optional<User> userOptional = service.findById(id);
+
+        if (userOptional.isPresent()) {
+            User userDb = userOptional.get();
+            userDb.setEmail(user.getEmail());
+            userDb.setLastname(user.getLastname());
+            userDb.setName(user.getName());
+            userDb.setPassword(user.getPassword());
+            userDb.setUsername(user.getUsername());
+            return ResponseEntity.ok(service.save(userDb));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Optional<User> userOptional = service.findById(id);
+
+        if (userOptional.isPresent()) {
+            service.deleteById(id);
+            ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.noContent().build();
     }
 
 }
