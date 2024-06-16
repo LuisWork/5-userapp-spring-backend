dcba903ba910cc9f31f2a1e55555903298e3e4d1
Luis Zambrano <LuisZambrano.Work@gmail.com>
2024-06-16 02:03:32 -0400

    Implementando UserService y UserServiceImpl

---
+++ src/main/java/com/springboot/backend/luis/usersapp/users_backend/services/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.springboot.backend.luis.usersapp.users_backend.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springboot.backend.luis.usersapp.users_backend.entities.User;
+import com.springboot.backend.luis.usersapp.users_backend.repositories.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return (List) this.repository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> findById(@NonNull Long id) {
+        return this.repository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public User save(User user) {
+        return this.repository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+}
