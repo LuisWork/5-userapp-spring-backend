441407b66a92e3c3e67e914f194aaaf632f5bca0
Luis Zambrano <LuisZambrano.Work@gmail.com>
2024-06-18 04:39:54 -0400

    Implementando ManyToMany en entity User

--- src/main/java/com/springboot/backend/luis/usersapp/users_backend/entities/User.java
+++ src/main/java/com/springboot/backend/luis/usersapp/users_backend/entities/User.java
@@ -1,10 +1,20 @@
 package com.springboot.backend.luis.usersapp.users_backend.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -33,6 +43,17 @@ public class User {
 
     @NotBlank
     private String password;
+
+    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "users_roles", joinColumns = {@JoinColumn(name = "user_id") },
+            inverseJoinColumns = @JoinColumn(name = "role_id"),
+            uniqueConstraints = {@UniqueConstraint(columnNames = { "user_id", "role_id" }) })
+    private List<Role> roles;
+
+    public User() {
+        this.roles = new ArrayList<>();
+    }
 
     public Long getId() {
         return id;
@@ -80,6 +101,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
     }
 
 }
